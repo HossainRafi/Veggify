8103001cfd5f5402ee6f4fbcfcf7a64129157ea1
HossainRafi <hossainrafi71@gmail.com>
2024-06-14 21:52:35 +0600

    hero text

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Hero = () => {
+  return (
+    <>
+      <div className="px-5 xl:px-10 md:w-1/2 mb-10">
+        <h1 className="mt-6 mb-10 text-4xl xl:text-5xl text-center font-bold leading-normal xl:leading-relaxed text-[#2A3342]">
+          A blog template made for food <span className="text-orange-500 italic">influencers</span>
+        </h1>
+      </div>
+    </>
+  );
+};
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,5 +1,15 @@
+import { Hero } from "../../components/Hero";
+
 const Home = () => {
-  return <>Home</>;
+  return (
+    <>
+      <div className="container mx-auto">
+        <div className="flex flex-col justify-center items-center w-full py-20">
+          <Hero />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
