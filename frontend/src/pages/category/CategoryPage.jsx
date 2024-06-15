96b657737eaeae66854223a7542588ae02631502
HossainRafi <hossainrafi71@gmail.com>
2024-06-15 10:31:22 +0600

    category heading

--- frontend/src/pages/category/CategoryPage.jsx
+++ frontend/src/pages/category/CategoryPage.jsx
@@ -2,10 +2,12 @@ import { useParams } from "react-router-dom";
 
 export const CategoryPage = () => {
   const { category } = useParams();
-  
+
   return (
     <div className="px6 lg:px-12 py-20">
-      <h1>{category}</h1>
+      <h1 className="text-center text-3xl py-10 font-semibold text-secondary sm:text-6xl sm:leading-relaxed capitalize">
+        {category}
+      </h1>
     </div>
   );
 };
