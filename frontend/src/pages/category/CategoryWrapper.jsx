9fcee9f912e82d057ad884861b0e74f579b861d0
HossainRafi <hossainrafi71@gmail.com>
2024-06-15 10:05:28 +0600

    category item

--- frontend/src/pages/category/CategoryWrapper.jsx
+++ frontend/src/pages/category/CategoryWrapper.jsx
@@ -1,10 +1,15 @@
 import { Link } from "react-router-dom";
 
-function CategoryItem() {
+function CategoryItem({ name, href, bgColor, color }) {
+  const style={
+    backgroundColor: bgColor,
+    color: color,
+    borderColor: color
+  }
   return (
     <div>
-      <Link to={"/"}>
-        <div>Name</div>
+      <Link to={href} className="rounded-full">
+        <div className="uppercase px-6 py-2 text-center rounded-full" style={style}>{name}</div>
       </Link>
     </div>
   );
@@ -13,7 +18,42 @@ function CategoryItem() {
 function CategoryList() {
   return (
     <div className="flex flex-wrap justify-center items-center gap-8">
-      <CategoryItem />
+      <CategoryItem
+        name="entrees"
+        href="/categories/entrees"
+        bgColor="#f0f5c4"
+        color="#59871f"
+      />
+      <CategoryItem
+        name="breakfast"
+        href="/categories/breakfast"
+        bgColor="#efedfa"
+        color="#3c3a8f"
+      />
+      <CategoryItem
+        name="lunch"
+        href="/categories/lunch"
+        bgColor="#e5f7f3"
+        color="#1f8787"
+      />
+      <CategoryItem
+        name="desserts"
+        href="/categories/desserts"
+        bgColor="#e8f5fa"
+        color="#397a9e"
+      />
+      <CategoryItem
+        name="sides"
+        href="/categories/sides"
+        bgColor="#feefc9"
+        color="#d16400"
+      />
+      <CategoryItem
+        name="drinks"
+        href="/categories/drinks"
+        bgColor="#ffeae3"
+        color="#fo493e"
+      />
     </div>
   );
 }
