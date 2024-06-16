c31a34f67f4d7ef0b1e5895dc2832749a791cbc6
HossainRafi <hossainrafi71@gmail.com>
2024-06-16 09:30:56 +0600

    database connected successfully

--- backend/index.js
+++ backend/index.js
@@ -8,12 +8,12 @@ const port = process.env.PORT || 5000;
 // hossainrafi71
 // 2zlp9QzuO4cXQiam
 
-main().catch((err) => console.log(err));
+
 
 async function main() {
   // mongodb connection
   await mongoose.connect(
-    "mongodb+srv://hossainrafi71:2zlp9QzuO4cXQiam@veggify.zvv7zdi.mongodb.net/?retryWrites=true&w=majority&appName=veggify"
+    "mongodb+srv://hossainrafi71:2zlp9QzuO4cXQiam@veggify.zvv7zdi.mongodb.net/veggify?retryWrites=true&w=majority&appName=veggify"
   );
 
   app.get("/", (req, res) => {
@@ -21,6 +21,13 @@ async function main() {
   });
 }
 
+main()
+  .then(() =>
+    console.log(`=================================================================================================
+    --------------------------- MongoDB Connected Successfully -----------------------------
+================================================================================================= `)
+  )
+  .catch((err) => console.log(err));
 
 // server running or not
 app.listen(port, () => {
@@ -28,4 +35,3 @@ app.listen(port, () => {
     ---------------------------- APP IS LISTENING ON PORT ${port} -----------------------------
 ================================================================================================= `);
 });
-
