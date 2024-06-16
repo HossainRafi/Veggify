ce872bc674401283aba7bf7d0e4ac152605d97d5
HossainRafi <hossainrafi71@gmail.com>
2024-06-16 09:07:20 +0600

    mongoose setup

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,21 @@
+// import packages
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+main().catch((err) => console.log(err));
 
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+
+  app.get("/", (req, res) => {
+    res.send("Veggify Recipe App Server Is Running !!");
+  });
+}
+
+
+// server running or not
 app.listen(port, () => {
   console.log(`=================================================================================================
     ---------------------------- APP IS LISTENING ON PORT ${port} -----------------------------
