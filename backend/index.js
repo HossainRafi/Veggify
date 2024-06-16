f4984d26941a44e63b40d4a0400d7dcad9982943
HossainRafi <hossainrafi71@gmail.com>
2024-06-16 18:02:06 +0600

    itemRoute

--- backend/index.js
+++ backend/index.js
@@ -2,16 +2,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
-
 
 // hossainrafi71
 // 2zlp9QzuO4cXQiam
 
+// middleware
+app.use(express.json());
+app.use(cors());
 
-
+// mongodb connection
 async function main() {
-  // mongodb connection
   await mongoose.connect(
     "mongodb+srv://hossainrafi71:2zlp9QzuO4cXQiam@veggify.zvv7zdi.mongodb.net/veggify?retryWrites=true&w=majority&appName=veggify"
   );
