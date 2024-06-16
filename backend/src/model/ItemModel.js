ea90363e3d6c4a54d0409cea96f0ae025c158932
HossainRafi <hossainrafi71@gmail.com>
2024-06-16 17:27:01 +0600

    commentSchema

--- backend/src/model/ItemModel.js
+++ backend/src/model/ItemModel.js
@@ -1,12 +1,25 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
+// ingredient details
 const ingredientSchema = new Schema({
   name: {
     type: String,
     required: true,
   },
   quantity: {
+    type: String,
+    required: true,
+  },
+});
+
+// comment details
+const commentSchema = new Schema({
+  user: {
+    type: String,
+    required: true,
+  },
+  comment: {
     type: String,
     required: true,
   },
