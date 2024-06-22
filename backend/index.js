bcb9a05c5c31dbf88e79a35e590fb2d98ac83503
HossainRafi <hossainrafi71@gmail.com>
2024-06-22 21:39:01 +0600

    category backend

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-// mongodb connection
+// connecting mongodb
 async function main() {
   await mongoose.connect(
     "mongodb+srv://hossainrafi71:2zlp9QzuO4cXQiam@veggify.zvv7zdi.mongodb.net/veggify?retryWrites=true&w=majority&appName=veggify"
@@ -23,6 +23,7 @@ async function main() {
   });
 }
 
+// mongodb connected or not
 main()
   .then(() =>
     console.log(`=================================================================================================
@@ -31,8 +32,12 @@ main()
   )
   .catch((err) => console.log(err));
 
-  const ItemRoutes=require('./src/routes/itemRoute')
-  app.use("/api", ItemRoutes);
+// routes
+const ItemRoutes = require("./src/routes/itemRoute");
+const CategoryRoutes = require("./src/routes/categoryRoute");
+
+app.use("/api", ItemRoutes);
+app.use("/api/", CategoryRoutes);
 
 // server running or not
 app.listen(port, () => {
--- backend/src/routes/categoryRoute.js
+++ backend/src/routes/categoryRoute.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const Category = require("../model/categoryModel");
 const router = express.Router();
 
-router.get("/categories/:category");
+const CategoryController = require("../controllers/categoryController");
+
+router.get("/categories/:category", CategoryController.getCategory);
 
 module.exports = router;
