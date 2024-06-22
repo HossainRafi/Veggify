108ff1473d1fe4aa57786a8f5dce82eeb5a1362a
HossainRafi <hossainrafi71@gmail.com>
2024-06-22 10:58:45 +0600

    search item

--- backend/src/controllers/itemController.js
+++ backend/src/controllers/itemController.js
@@ -1,12 +1,27 @@
-const Item= require("../model/ItemModel")
+const Item = require("../model/ItemModel");
 
 // get all items
-const getAllItems=async (req, res)=>{
-const result = await Item.find().sort({createAt: -1})
-res.status(200).json(result)
-}
+const getAllItems = async (req, res) => {
+  const result = await Item.find().sort({ createAt: -1 });
+  res.status(200).json(result);
+};
+
+// get searched items
+const getSearchedItems = async (req, res) => {
+  const { q } = req.query;
+  try {
+    let items;
+    if (q) {
+      items = await Item.find({ name: { $regex: q, $option: "i" } });
+    }
+    res.status(200).json(items);
+  } catch (error) {
+    res.status(500).json({ message: "No items found !" });
+  }
+};
 
 // exports
-module.exports={
-  getAllItems
-}
+module.exports = {
+  getAllItems,
+  getSearchedItems,
+};
