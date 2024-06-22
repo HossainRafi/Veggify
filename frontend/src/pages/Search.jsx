caed666ec06f512cfdfe85311a3c66a7e62303fd
HossainRafi <hossainrafi71@gmail.com>
2024-06-22 18:13:15 +0600

    states

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -1,7 +1,24 @@
 import React from "react";
-import { IoSearchOutline } from 'react-icons/io5';
+import { useState } from "react";
+import { IoSearchOutline } from "react-icons/io5";
+import { useParams } from "react-router-dom";
 
 export const Search = () => {
+  const searchText = useParams();
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+
+
+
+
+
+
+
+  
+
   return (
     <div className="px-6 lg:px-12 py-20">
       <h1 className="text-center text-3xl py-10 font-semibold text-secondary sm:text-6xl sm:leading-relaxed">
@@ -19,6 +36,8 @@ export const Search = () => {
           className="outline-none w-full placeholder:text-neutral-500"
         />
       </div>
+
+      <ul></ul>
     </div>
   );
 };
