307e440ce897a140a8e37599b25558a2f88f32e2
HossainRafi <hossainrafi71@gmail.com>
2024-06-23 15:27:44 +0600

    footer done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import Header from "./components/header/Header";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <div className="max-w-screen-xl mx-auto">
       <Header />
 
-      <Outlet />
+      <div className="min-h-[calc(100vh-136px)]">
+        <Outlet />
+      </div>
+
+      <Footer />
     </div>
   );
 }
