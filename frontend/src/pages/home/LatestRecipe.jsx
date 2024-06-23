caab5c8063e3e0b7650981e8c758b507bf403d5f
HossainRafi <hossainrafi71@gmail.com>
2024-06-23 13:32:55 +0600

    latest recipes done

--- frontend/src/pages/home/LatestRecipe.jsx
+++ frontend/src/pages/home/LatestRecipe.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Card } from "../../components/Card";
+import { Link } from "react-router-dom";
 
 export default function LatestRecipe() {
   const [items, setItems] = useState([]);
@@ -21,14 +22,22 @@ export default function LatestRecipe() {
       </h2>
 
       {/* get all items */}
-      <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
         {items.length > 0 ? (
           items
-            .slice(0, 4)
+            .slice(0, 8)
             .map((item, index) => <Card key={item._id} item={item} />)
         ) : (
           <p>Loading........</p>
         )}
+      </div>
+
+      <div className="sm:w-64 mx-auto mt-16">
+        <Link to="/recipies">
+          <button className="py-4 px-8 hover:bg-btnColor text-secondary hover:text-white w-full transition ease-in duration-200 text-center text-base font-semibold border border-[#9c702a] focus:outline-none rounded-lg">
+            View Recipes
+          </button>
+        </Link>
       </div>
     </div>
   );
