9505f8d2c5b891c1967e08abc9cc020ad507d25d
HossainRafi <hossainrafi71@gmail.com>
2024-06-24 09:14:05 +0600

    contact right

--- frontend/src/pages/contact/Contact.jsx
+++ frontend/src/pages/contact/Contact.jsx
@@ -110,7 +110,60 @@ export const Contact = () => {
                 </ul>
               </div>
 
-              
+              {/* contact right */}
+              <div className="card h-fit max-w-6xl p-5 md:p-12" id="form">
+                <h2 className="mb-4 text-2xl font-bold">
+                  Ready to Get Started?
+                </h2>
+                <form id="contactForm">
+                  <div className="mb-6">
+                    <div className="mx-0 mb-1 sm:mb-4">
+                      <div className="mx-0 mb-1 sm:mb-4">
+                        <label
+                          htmlFor="name"
+                          className="pb-1 text-xs uppercase tracking-wider"
+                        ></label>
+                        <input
+                          type="text"
+                          id="name"
+                          autoComplete="given-name"
+                          placeholder="Your name"
+                          className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md sm:mb-0 outline-none"
+                          name="name"
+                        />
+                      </div>
+                      <div className="mx-0 mb-1 sm:mb-4">
+                        <label
+                          htmlFor="email"
+                          className="pb-1 text-xs uppercase tracking-wider"
+                        ></label>
+                        <input
+                          type="email"
+                          id="email"
+                          autoComplete="email"
+                          placeholder="Your email address"
+                          className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md sm:mb-0 outline-none"
+                          name="email"
+                        />
+                      </div>
+                    </div>
+                    <div className="mx-0 mb-1 sm:mb-4">
+                      <label
+                        htmlFor="textarea"
+                        className="pb-1 text-xs uppercase tracking-wider"
+                      ></label>
+                      <textarea
+                        id="textarea"
+                        name="textarea"
+                        cols="30"
+                        rows="5"
+                        placeholder="Write your message..."
+                        className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md sm:mb-0 outline-none resize-none"
+                      ></textarea>
+                    </div>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
