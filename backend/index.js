8e8f437343e0979694801e9541064c35d233fc91
HossainRafi <hossainrafi71@gmail.com>
2024-06-25 12:13:23 +0600

    cors update

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,13 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 const dotenv = require("dotenv").config();
 
+const corsOptions = {
+  origin: ["https://veggify-ct6h.vercel.app", "http://localhost:5173"],
+};
+
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 // connecting mongodb
 async function main() {
